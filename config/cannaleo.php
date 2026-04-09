9a7f0809b740afbe350918cd650c4ab7e0092142
rahuldhopte01 <rahuldhopte01@users.noreply.github.com>
2026-04-09 20:44:52 +0530

    worked on the prescription logic

--- config/cannaleo.php
+++ config/cannaleo.php
@@ -22,6 +22,7 @@ return [
     |--------------------------------------------------------------------------
     */
     'prescription_api_path' => env('CUROBO_PRESCRIPTION_API_PATH', '/api/v1/prescription/'),
+    'curobo_domain' => env('CUROBO_DOMAIN', ''),
     'prescription_callback_url' => env('CUROBO_PRESCRIPTION_CALLBACK_URL', ''),
     'default_signature_city' => env('CUROBO_DEFAULT_SIGNATURE_CITY', ''),
 
